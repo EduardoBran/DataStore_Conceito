a04846bad0da4f6042279ade1292a0cd68e0ee95
EduardoBran <eduardo.ads1814@gmail.com>
2025-07-01 12:05:35 -0300

    adicionando dependencia para uso do DataStore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,4 +48,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    // DataStore
+    implementation("androidx.datastore:datastore-preferences:1.1.4")
 }
